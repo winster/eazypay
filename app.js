29a6b6ed6fd79614baea8f564311e90175714034
Winster Thuruthippillil JOSE <wtjose@gmail.com>
2017-04-08 17:00:53 +0530

    returing the response

--- app.js
+++ app.js
@@ -31,11 +31,13 @@ app.post('/message', function(request, response) {
     res.setEncoding('utf8');
     res.on('data', function (chunk) {
       console.log('BODY: ' + chunk);
+      response.json({'result':'success'});
     });
   });
 
   fcm_req.on('error', function(e) {
     console.log('problem with request: ' + e.message);
+    response.json({'result':'failed'});
   });
 
   // write data to request body
